73e33b4b95077e033af1a3e6edd455fab05b4dee
galinababenka23-sudo <galina.babenka23@gmail.com>
2026-04-02 17:25:37 +0300

    Update backup.sh

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Простой бэкап папки с датой
-backup_dir="$HOME/backup_$(date +%Y%m%d_%H%M%S)"
+backup_dir="$HOME/backup_$(date +%Y%m%d)"
 mkdir -p "$backup_dir"
 cp -r "$1" "$backup_dir"
-echo "Бэкап создан: $backup_dir"
+echo "Бэкап создан в $backup_dir"
